fd1638f70d9f47fa4911a0a8db8b8f32ae453535
Toàn Tạ <taquoctoan1412@gmail.com>
2023-05-22 21:51:39 +0700

    Nop bai tuan 1

---
+++ src/main/java/com/example/textLab3/TextLab3Application.java
@@ -0,0 +1,13 @@
+package com.example.textLab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextLab3Application.class, args);
+	}
+
+}
